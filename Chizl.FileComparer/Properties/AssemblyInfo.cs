54200641723b4e6ec3c702d31b121586e82b35c0
Switch <31280229+gavin1970@users.noreply.github.com>
2025-09-21 06:24:32 -0400

    Version

--- Chizl.FileComparer/Properties/AssemblyInfo.cs
+++ Chizl.FileComparer/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 // 
 [assembly: AssemblyVersion("5.9.0.0")]
-[assembly: AssemblyFileVersion("5.9.21.612")]
+[assembly: AssemblyFileVersion("5.9.21.623")]
 [assembly: AssemblyInformationalVersion("5.9.21.0")]
 [assembly: AssemblyKeyFileAttribute("")]
 [assembly: AssemblyDelaySignAttribute(false)]
